a1196826e715f583a63de25813c0f9315fb1b1f7
paulthecheese79 <paolo.sangabriel@ciit.edu.ph>
2025-09-25 01:15:34 +0800

    Added Radio Buttons

--- app/src/main/java/com/example/signup/MainActivity.kt
+++ app/src/main/java/com/example/signup/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.signup
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +18,12 @@ class MainActivity : AppCompatActivity() {
         val usernameField = findViewById<EditText>(R.id.editTextUsername)
         val emailField = findViewById<EditText>(R.id.editTextEmail)
         val passwordField = findViewById<EditText>(R.id.editTextPassword)
+        val confirmPasswordField = findViewById<EditText>(R.id.editTextConfirmPassword)
+        val birthdateField = findViewById<TextInputEditText>(R.id.editTextBirthdate)
+        val genderGroup = findViewById<RadioGroup>(R.id.radioGroupGender)
         val signUpButton = findViewById<Button>(R.id.buttonSignUp)
-        val birthdateField = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.editTextBirthdate)
 
+        // 📅 Birthdate picker
         birthdateField.setOnClickListener {
             val datePicker =
                 MaterialDatePicker.Builder.datePicker()
@@ -25,9 +31,8 @@ class MainActivity : AppCompatActivity() {
                     .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
                     .build()
 
-            datePicker.addOnPositiveButtonClickListener { selection ->
-                // selection is a timestamp (milliseconds since epoch)
-                birthdateField.setText(datePicker.headerText) // headerText = nice formatted date
+            datePicker.addOnPositiveButtonClickListener {
+                birthdateField.setText(datePicker.headerText)
             }
 
             datePicker.show(supportFragmentManager, "birthdate_picker")
@@ -38,12 +43,28 @@ class MainActivity : AppCompatActivity() {
             val username = usernameField.text.toString()
             val email = emailField.text.toString()
             val password = passwordField.text.toString()
+            val confirmPassword = confirmPasswordField.text.toString()
 
-            // For now, just show a Toast
+            // Get selected gender
+            val selectedGenderId = genderGroup.checkedRadioButtonId
+            val gender = if (selectedGenderId != -1) {
+                val radioButton = findViewById<RadioButton>(selectedGenderId)
+                radioButton.text.toString()
+            } else {
+                "Not Selected"
+            }
+
+            // Show all input values in a Toast
             Toast.makeText(
                 this,
-                "Signing up with:\n$username\n$email\n$password",
-                Toast.LENGTH_SHORT
+                "Signing up with:\n" +
+                        "Username: $username\n" +
+                        "Email: $email\n" +
+                        "Password: $password\n" +
+                        "Confirm Password: $confirmPassword\n" +
+                        "Birthdate: ${birthdateField.text}\n" +
+                        "Gender: $gender",
+                Toast.LENGTH_LONG
             ).show()
         }
     }
